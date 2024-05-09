29fed531d5cb86d95a907eb0388352f1d24b9db0
Apolos666 <quangprox121@gmail.com>
2024-05-09 10:24:33 +0700

    feat: complete login ui

---
+++ Controllers/AuthenticateController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharp_FinalExam.Controllers;
+
+[Route("authenticate")]
+[AllowAnonymous]
+public class AuthenticateController : Controller
+{
+    [Route("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+}

08ce5c4b41084dbd8ad93b43abd932611ec0e862
Apolos666 <quangprox121@gmail.com>
2024-05-10 14:25:36 +0700

    feat(security): add roles based authorize to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CSharp_FinalExam.Models;
+using CSharp_FinalExam.Utilities.TypeSafe;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CSharp_FinalExam.Controllers;
 
+[Authorize(Roles = TypeSafe.Roles.Admin)]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;

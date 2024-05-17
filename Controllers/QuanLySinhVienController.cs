f8735d95edd71c712898f5e8db238be37aeb20e7
Apolos666 <quangprox121@gmail.com>
2024-05-17 15:40:11 +0700

    feat: Complete the interface template

---
+++ Controllers/QuanLySinhVienController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharp_FinalExam.Controllers;
+
+public class QuanLySinhVienController : Controller
+{
+    public QuanLySinhVienController()
+    {
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

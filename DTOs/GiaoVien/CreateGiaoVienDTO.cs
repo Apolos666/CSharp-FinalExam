bdee8fd34ee058cdfcc09ef6cf4e3229d2bb6a64
Apolos666 <quangprox121@gmail.com>
2024-05-19 00:27:15 +0700

    feat: clean: Add display name to CreateGiaoVienDTO property so it displays information in Vietnamese

--- DTOs/GiaoVien/CreateGiaoVienDTO.cs
+++ DTOs/GiaoVien/CreateGiaoVienDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using CSharp_FinalExam.Infrastructure.CustomValidation;
 using CSharp_FinalExam.Models;
@@ -8,13 +9,14 @@ namespace CSharp_FinalExam.DTOs.GiaoVien;
 public class CreateGiaoVienDTO
 { 
     [Required]
+    [DisplayName("Họ tên")]
     [StringLength(50, MinimumLength = 10, ErrorMessage = "Họ tên sinh viên phải có từ 10 đến 50 ký tự.")]
     public string HoTen { get; set; }
 
-    [Required] [DateVietFormat] public DateTime NgaySinh { get; set; }
-    [Required] [VietPhoneNumber] public string SoDienThoai { get; set; }
+    [Required] [DisplayName("Ngày sinh")] [DateVietFormat] public DateTime NgaySinh { get; set; }
+    [Required] [DisplayName("Số điện thoại")] [VietPhoneNumber] public string SoDienThoai { get; set; }
     [Required] [EmailAddress] public string Email { get; set; }
-    [Required] public string DiaChi { get; set; }
-    [Required] public GioiTinhEnum GioiTinh { get; set; }
-    [Required] [ForeignKey("Khoa")] public int KhoaId { get; set; }
+    [Required] [DisplayName("Địa chỉ")] public string DiaChi { get; set; }
+    [Required] [DisplayName("Giới tính")] public GioiTinhEnum GioiTinh { get; set; }
+    [Required] [DisplayName("Khoa")] [ForeignKey("Khoa")] public int KhoaId { get; set; }
 }

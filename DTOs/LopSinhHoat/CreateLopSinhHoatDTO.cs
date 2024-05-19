33799e65a3ca39d46870f297a2e7f70145c74117
Apolos666 <quangprox121@gmail.com>
2024-05-19 14:22:59 +0700

    feat: add displayname attribute to CreateLopSinhHoatDTO so it displays titile in Vietnamese

--- DTOs/LopSinhHoat/CreateLopSinhHoatDTO.cs
+++ DTOs/LopSinhHoat/CreateLopSinhHoatDTO.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSharp_FinalExam.DTOs.LopSinhHoat;
 
 public class CreateLopSinhHoatDTO
 {
-    [Required] public string TenLop { get; set; }
-    [Required] public int HocKy { get; set; }
-    [Required] public int GiaoVienChuNhiemId { get; set; }
-    [Required] public int KhoaId { get; set; }
+    [Required] [DisplayName("Tên lớp")] public string TenLop { get; set; }
+    [Required] [DisplayName("Học kỳ")] public int HocKy { get; set; }
+    [Required] [DisplayName("Giáo viên chủ nhiệm id")] public int GiaoVienChuNhiemId { get; set; }
+    [Required] [DisplayName("Khoa id")] public int KhoaId { get; set; }
 }

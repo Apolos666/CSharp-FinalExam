5931f2dcf1a856440bca917e8f8be5e23e3f27ac
Apolos666 <quangprox121@gmail.com>
2024-05-19 14:07:31 +0700

    feat: complete crud for table LopSinhHoat

---
+++ DTOs/LopSinhHoat/CreateLopSinhHoatDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharp_FinalExam.DTOs.LopSinhHoat;
+
+public class CreateLopSinhHoatDTO
+{
+    [Required] public string TenLop { get; set; }
+    [Required] public int HocKy { get; set; }
+    [Required] public int GiaoVienChuNhiemId { get; set; }
+    [Required] public int KhoaId { get; set; }
+}
---
+++ Repositories/LopSinhHoat/LopSinhHoatRepository.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using CSharp_FinalExam.Data;
+using CSharp_FinalExam.DTOs.LopSinhHoat;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharp_FinalExam.Repositories.LopSinhHoat;
+
+public class LopSinhHoatRepository : ILopSinhHoatRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public LopSinhHoatRepository(ApplicationDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+    
+    public async Task<IEnumerable<Models.LopSinhHoat>?> GetAllLopSinhHoatAsync()
+    {
+        var lopSinhHoats = await _dbContext.LopSinhHoats.ToListAsync();
+        
+        if (lopSinhHoats.Count == 0)
+            return null;
+        
+        return lopSinhHoats;
+    }
+
+    public async Task<Models.LopSinhHoat?> GetLopSinhHoatByIdAsync(int id)
+    {
+        var lopSinhHoat = await _dbContext.LopSinhHoats.FindAsync(id);
+        
+        if (lopSinhHoat is null)
+            return null;
+        
+        return lopSinhHoat;
+    }
+
+    public async Task<Models.LopSinhHoat> CreateLopSinhHoatAsync(CreateLopSinhHoatDTO createLopSinhHoatDto)
+    {
+        var lopSinhHoat = _mapper.Map<Models.LopSinhHoat>(createLopSinhHoatDto);
+        var addResult = await _dbContext.LopSinhHoats.AddAsync(lopSinhHoat);
+        await _dbContext.SaveChangesAsync();
+        return addResult.Entity;
+    }
+
+    public async Task<Models.LopSinhHoat> UpdateLopSinhHoatAsync(Models.LopSinhHoat updateLopSinhHoat, int id)
+    {
+        var lopSinhHoat = await GetLopSinhHoatByIdAsync(id);
+        _mapper.Map(updateLopSinhHoat, lopSinhHoat);
+        await _dbContext.SaveChangesAsync();
+        return lopSinhHoat;
+    }
+
+    public async Task<bool> DeleteLopSinhHoatAsync(int id)
+    {
+        var lopSinhHoat = await GetLopSinhHoatByIdAsync(id);
+        
+        if (lopSinhHoat is null)
+            return false;
+        
+        _dbContext.LopSinhHoats.Remove(lopSinhHoat);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+}
